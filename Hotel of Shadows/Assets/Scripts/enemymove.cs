5ab8142a83918de8093f5ba35300737386b11cbf
Ayratos <129599339+Ayratos@users.noreply.github.com>
2024-02-11 15:46:55 +0300

    ghost ai, dialogue fix, fog

---
+++ Hotel of Shadows/Assets/Scripts/enemymove.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.AI;
+
+
+
+public class enemymove : MonoBehaviour
+
+{
+
+    private Transform Player;
+
+
+
+    private NavMeshAgent agent;
+
+    public float MoveSpeed;
+    public float MaxDist = 0.7f;
+    public float MinDist = 0;
+    public float enemyDistance = 0.7f;
+
+
+
+    private void Start()
+
+    {
+
+        Player = GameObject.FindWithTag("Player").transform;
+
+
+
+        //agent = GetComponent<NavMeshAgent>();
+
+    }
+
+
+
+    //Call every frame
+
+    void Update()
+
+    {
+
+        //Look at the player
+
+        transform.LookAt(Player);
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+
+            if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+            {
+                Destroy(this.gameObject);
+
+            }
+        }
+    }
+}
+
